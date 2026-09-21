b34524339e6c5652456e65eff9be55520b27297e
kujo-jojo <adababio83@gmail.com>
2026-09-21 00:37:44 -0400

    Add service worker with install and activate events

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = "qikari-v1";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
